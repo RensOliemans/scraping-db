ebc636ce875a1ecb42c234af1d3994b119e98a76
Rens Oliemans <rens.oliemans@gmail.com>
2019-11-07 14:29:56 +0100

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Installation Instructions
+
+Requires `python3.6+`
+On new virtualenv, run `pip install -r requirements.txt`
+
+Create a `.env` with relevant entries (see `scrape_db.py`)
+
+Run with `python scrape_db.py`
---
+++ scrape_db.py
@@ -0,0 +1,88 @@
+import csv
+import os
+import sys
+
+import psycopg2
+from dotenv import load_dotenv
+from loguru import logger
+
+load_dotenv()
+
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+DB_NAME = os.getenv("DB_NAME")
+USER = os.getenv("DB_USER")
+PASSWORD = os.getenv("DB_PASSWORD")
+
+FILENAME = 'wifiexport.csv'
+TIME_START = '2018-09-01'
+TIME_END = '2018-09-02'
+
+
+@logger.catch
+def connect_to_db(host, database, port, user, password):
+    logger.debug("Connecting to database")
+    connect = psycopg2.connect(host=host, database=database, port=port,
+                               user=user, password=password)
+    logger.debug("Connected to database")
+
+    return connect
+
+
+@logger.catch
+def perform_query(cursor, time_start, time_end):
+    sql_query = f"SELECT * \
+                  FROM wifidata.association_counts \
+                  WHERE time >= '{time_start}' \
+                       AND time <= '{time_start}'"
+
+    logger.debug("Fetching results")
+    cursor.execute(sql_query)
+
+    headers = [i[0] for i in cursor.description]
+    results = cursor.fetchall()
+    logger.debug("Obtained results")
+
+    return headers, results
+
+
+@logger.catch
+def write_to_csv(filename, headers, results):
+    logger.debug("Opening csv file")
+    csv_file = csv.writer(open(filename, 'w', newline=''),
+                          delimiter=',', lineterminator='\n')
+
+    logger.debug("Writing headers")
+    csv_file.writerow(headers)
+    logger.debug("Writing rows")
+    csv_file.writerows(results)
+
+    logger.debug("Writing successful")
+
+
+def setup_db_connection():
+    connect = connect_to_db(HOST, DB_NAME, PORT, USER, PASSWORD)
+    cursor = connect.cursor()
+
+    return connect, cursor
+
+def import_data(cursor):
+    headers, results = perform_query(cursor, TIME_START, TIME_END)
+    return (headers, results)
+
+def export_data(filename, data):
+    headers, results = data
+    write_to_csv(filename, headers, results)
+
+def cleanup_db_connection(connect):
+    connect.close()
+
+
+def main():
+    connect, cursor = setup_db_connection()
+    data = import_data(cursor)
+    export_data(FILENAME, data)
+    cleanup_db_connection(connect)
+    connect.close()
+
+main()
